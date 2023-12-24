6d9b9c577f24263ef04c24d92629dad1e036e980
Bouali-Salma <salmabouali2610@gmail.com>
2023-12-24 22:25:09 +0100

    department view added

--- IT_management/__manifest__.py
+++ IT_management/__manifest__.py
@@ -2,7 +2,8 @@
  'name':'parc_informatique',
  'author':'Bouali Salma - Errahouti Aissam',
  'data': ['views/asset_view.xml',
-            'views/user_view.xml'
+            'views/user_view.xml',
+            'views/department_view.xml'
 
 ]
 }
--- IT_management/models/departement.py
+++ IT_management/models/departement.py
@@ -4,6 +4,7 @@ class ITDepartment(models.Model):
     _name = 'parc_informatique.department'
     _description = 'IT Department Model'
 
-    name = fields.Char(string='Department Name', required=True)
+    name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
-    users = fields.One2many('parc_informatique.user', 'department', string='Users')
+    users = fields.Many2many('parc_informatique.user', string='Users')
+    assets = fields.Many2many('parc_informatique.asset', string='Assets')
