fca65e94d38fc9197ea6da9e88b3fe27738ecd1d
Errahouti <milbournato@gmail.com>
2023-12-24 22:53:26 +0100

    software view

--- IT_management/__manifest__.py
+++ IT_management/__manifest__.py
@@ -3,7 +3,8 @@
  'author':'Bouali Salma - Errahouti Aissam',
  'data': ['views/asset_view.xml',
             'views/user_view.xml',
-            'views/hardware_view.xml'
+            'views/hardware_view.xml',
+            'views/software_view.xml'
 
 
 
