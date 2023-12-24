77e3ba904b22d2052f44e77be1c534c9cd02a4d1
Bouali-Salma <salmabouali2610@gmail.com>
2023-12-24 23:28:40 +0100

    inventory view

--- IT_management/__manifest__.py
+++ IT_management/__manifest__.py
@@ -3,8 +3,9 @@
  'author':'Bouali Salma - Errahouti Aissam',
  'data': ['views/asset_view.xml',
             'views/user_view.xml',
-            'views/department_view.xml'
-            'views/hardware_view.xml'
+            'views/department_view.xml',
+            'views/hardware_view.xml',
+            'views/inventory_view.xml'
 
 ]
 }
