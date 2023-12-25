648e250e63b951b6c16b4619af232250a3e98b31
Errahouti <milbournato@gmail.com>
2023-12-25 19:33:42 +0100

    view and attributes changes

--- IT_management/__manifest__.py
+++ IT_management/__manifest__.py
@@ -1,13 +1,19 @@
 {
- 'name':'parc_informatique',
- 'author':'Bouali Salma - Errahouti Aissam',
+ 'name': 'parc_informatique',
+ 'version': '1.0.0',
+ 'author': 'Bouali Salma & Errahouti Aissam',
+ 'category': 'IT Asset Management',
+ 'summary': 'A Module For IT Asset Management',
+ 'images': [
+     'static/description/icon.jpg'
+ ],
  'depends': ['base', 'hr'],
- 'data': ['views/asset_view.xml',
-            'views/user_view.xml',
-            'views/inventory_view.xml',
-            'views/hardware_view.xml',
-            'views/software_view.xml',
-            'views/maintenance_view.xml',
-
-]
+ 'data': [
+     'views/asset_view.xml',
+     'views/user_view.xml',
+     'views/inventory_view.xml',
+     'views/hardware_view.xml',
+     'views/software_view.xml',
+     'views/maintenance_view.xml',
+ ]
 }
--- IT_management/models/maintenance.py
+++ IT_management/models/maintenance.py
@@ -4,7 +4,17 @@ class Maintenance(models.Model):
     _name = 'parc_informatique.maintenance'
 
     maintenance_date = fields.Date(string='Maintenance Date')
-    maintenance_type = fields.Char(string='Maintenance Type')
+    maintenance_type = fields.Selection([
+        ('preventive', 'Preventive Maintenance'),
+        ('corrective', 'Corrective Maintenance'),
+        ('predictive', 'Predictive Maintenance'),
+        ('emergency', 'Emergency Maintenance'),
+        ('modernization', 'Modernization'),
+        ('testing_validation', 'Testing and Validation'),
+        ('other', 'Other'),
+    ], string='Maintenance Type') 
+
+
     description = fields.Text(string='Description')
     cost = fields.Float(string='Cost')
     asset_id = fields.Many2one('parc_informatique.asset', string='Asset')
